e445725c252fb5499c910a4c11d6afc7cf1163c0
Freitas <tmsi.freitas@gmail.com>
2019-05-05 23:13:24 -0300

    news resolutions

--- mundo3/ex075.py
+++ mundo3/ex075.py
@@ -12,11 +12,18 @@ val5 = int(input('Digite o 5º valor: '))
 
 conj = (val1, val2, val3, val4, val5)
 
-qtd9 = 0
-par = 0
-for numero in conj:
-    if numero == 9:
-        qtd9 +=1
-    
+#qtd9 = 0
+#par = 0
+#for numero in conj:
+#    if numero == 9:
+#        qtd9 +=1
 
-print(f'Digitaram nove {qtd9} vezes')
+#print(f'Digitaram nove {qtd9} vezes')
+print(f'Digitaram nove {conj.count(9)} vezes')
+if 3 in conj:
+    print(f'O primeiro 3 aparece na posição {conj.index(3)}')
+print('Os valores pares são: ', end='')
+for num in conj:
+    if num % 2 == 0:
+        print(f'{num}', end=' ')
+print()
--- mundo3/ex077.py
+++ mundo3/ex077.py
@@ -1,3 +1,4 @@
 # Crie um programa que tenha uma tupla com varias palavras (não usar
 # acentos). Depois disso, voce deve mostrar, para cada palavra, quais
 # são as suas vogais
+
