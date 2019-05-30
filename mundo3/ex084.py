2c500221315b7d2a16c8801c844f08f47ca927fa
Freitas <tmsi.freitas@gmail.com>
2019-05-30 00:11:19 -0300

    ~> New files

--- mundo3/ex084.py
+++ mundo3/ex084.py
@@ -3,3 +3,33 @@
 # a) Quantas pessoas foram cadastradas
 # b) Uma listagem com as pessoas mais pesadas.
 # c) Uma listagem com as pessoas mais leves.
+
+cadastros = list()
+pessoas = list()
+qtdePessoas = 0
+maisLeve = []
+maisPesado = []
+
+while True:
+    nome = str(input('Digite o nome: '))
+    peso = float(input('Digite o peso: '))
+    pessoas.append(nome)
+    pessoas.append(peso)
+    cadastros.append(pessoas[:])
+    pessoas.clear()
+    qtdePessoas += 1
+
+    continua = input('Deseja continuar? S/N ').upper()
+    if continua == 'N':
+        break
+
+for pessoa,peso in cadastros:
+    print(pessoa, peso)
+    if len(maisLeve) == 0:
+        maisLeve.append(nome)
+        maisLeve.append(peso)
+
+
+print(maisLeve)
+#print(f'Foram cadastradas {qtdePessoas} pessoas.')
+#print(f'{leves} foram os mais leves com {pesoLeve}')
